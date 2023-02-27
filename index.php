fbc51c25ef919acda0ac268f503db734020791da
Noam Taub <ntaub2@gmail.com>
2023-02-27 12:38:35 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+
+function createTable()
+{
+    $db = new JsonDatabase();
+
+    try {
+        $db->crateTable("customers");
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+
+function failCreateTable()
+{
+    $db = new JsonDatabase();
+
+    $db->crateTable("customers-fail");
+    try {
+        $db->crateTable("customers-fail");
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+
+function insertOneRow()
+{
+    $db = new JsonDatabase();
+    $data = [
+        "name" => "Noam",
+        "last" => "taub"
+    ];
+    try {
+        $db->insert("users", $data);
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+
+function insertMultipleRows()
+{
+    $db = new JsonDatabase();
+    $data = [
+        [
+            "name" => "Moshe",
+            "last" => "taub"
+        ],
+        [
+            "name" => "Noam",
+            "last" => "taub"
+        ],
+        [
+            "name" => "Tzvi",
+            "last" => "taub"
+        ],
+    ];
+    try {
+        $db->insert("users", $data);
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+
+function updateRow()
+{
+    $db = new JsonDatabase();
+
+    $data = [
+        "name" => "Avi",
+        "last" => "Katz"
+    ];
+
+    try {
+        $db->update('users', 2, $data);
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+function deleteRow()
+{
+    $db = new JsonDatabase();
+
+    try {
+        $db->delete('users', 1);
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+
+function findRow(){
+    $db = new JsonDatabase();
+
+    try {
+        $db->find('users', 2);
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
+function findAll(){
+    $db = new JsonDatabase();
+
+    try {
+        $db->findAll('users');
+    } catch (Exception $e) {
+        print_r($e->getMessage());
+    }
+}
